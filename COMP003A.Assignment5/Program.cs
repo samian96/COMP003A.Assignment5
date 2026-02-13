6ba4855ea4101b03e091031a4c9f2833979a5521
samian <sam.iannazzo@yahoo.com>
2026-02-13 00:16:30 -0800

    added bol to set true or false for running program

--- COMP003A.Assignment5/Program.cs
+++ COMP003A.Assignment5/Program.cs
@@ -1,36 +1,45 @@
-﻿namespace COMP003A.Assignment5
+﻿using System.Xml.Serialization;
+
+namespace COMP003A.Assignment5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int mainValue = 15;
-
-            DisplayMenu();
-
-            Console.Write("Please enter your name: ");
-            string name = Console.ReadLine();
-
-            Console.Write("Please enter your age: ");
-            string age = Console.ReadLine();
-
-            if (int.TryParse(age, out int inputAge))
+            bool running = true;
+            do
             {
-                UserInput(name, inputAge);
-            }
-            else
-            {
-                Console.WriteLine("Invalid number input.");
-            }
 
-            Console.Write("Please enter a number: ");
-            string num1= Console.ReadLine();
+                int mainValue = 15;
 
-            int finalVal = DoubleCalculation(mainValue);
+                DisplayMenu();
 
-            Console.Write($"Original Value: {mainValue}.");
+                Console.Write("Please enter your name: ");
+                string name = Console.ReadLine();
 
-            Console.Write($"Original Value times 2: {finalVal}");
+                Console.Write("Please enter your age: ");
+                string age = Console.ReadLine();
+
+                if (int.TryParse(age, out int inputAge))
+                {
+                    UserInput(name, inputAge);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid number input.");
+                }
+
+                Console.Write("Please enter a number: ");
+                string num1 = Console.ReadLine();
+
+                int finalVal = DoubleCalculation(mainValue);
+
+                Console.WriteLine($"Original Value: {mainValue}.");
+
+                Console.WriteLine($"Original Value doubled: {finalVal}");
+            } while (running == true);
+
+            Console.WriteLine("Program ended");
 
         }
         static void DisplayMenu()
