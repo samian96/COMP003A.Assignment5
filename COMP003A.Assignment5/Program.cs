38238cd93cc25e32ac1c5f75db0f22e5afb700e4
samian <sam.iannazzo@yahoo.com>
2026-02-12 20:08:46 -0800

    started building method userInput/ added readlines to main

--- COMP003A.Assignment5/Program.cs
+++ COMP003A.Assignment5/Program.cs
@@ -5,10 +5,21 @@
         static void Main(string[] args)
         {
             DisplayMenu();
+
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Please enter your age: ");
+            string age = Console.ReadLine();
+
         }
         static void DisplayMenu()
         {
             Console.WriteLine("This is the introduction menu to this program.");
+        }
+        static void UserInput(string name, int age)
+        {
+            
         }
     }
 }
